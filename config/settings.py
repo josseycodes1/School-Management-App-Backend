07ffc696f0922f6169874e403c21a1eac57c1c29
josseycodes1 <adewumijosephine1@gmail.com>
2025-08-24 15:16:06 +0100

    whitnoise config

--- config/settings.py
+++ config/settings.py
@@ -153,6 +153,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+
 # Media files
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
